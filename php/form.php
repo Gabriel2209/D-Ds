8e0299a293729631a98f65816e072c954034da36
Omargs <omargs1998@gmail.com>
2026-03-24 22:37:54 -0500

    php

--- php/form.php
+++ php/form.php
@@ -2,16 +2,44 @@
 include "conn.php";
 $pdo = new Conn();
 
+if(isset($_GET['editar'])){
+    $id = $_GET['editar'];
+    try{
+        $sql = "SELECT * FROM t_datos_personales WHERE id_persona = :id";
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindParam(':id',$id);
+        $stmt->execute();
+
+        $persona = $stmt->fetch(PDO::FETCH_ASSOC);
+    }catch(PDOException $e){
+        echo "Algo salio mal al editar ". $e->getMessage();
+    }
+
+}
+if(isset($_GET['eliminar'])){
+    $id = $_GET['eliminar'];
+
+    try{
+        $sql = "DELETE FROM t_datos_personales WHERE id_persona = :id";
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindParam(":id", $id);
+        $stmt->execute();
+    }catch(PDOException $e){
+        echo "Error al eliminar: " . $e->getMessage();
+    }
+}
 if(isset($_POST['btnRegistrarse'])){
     $nombre = "";
     $cedula = "";
     $email = "";
     $password = "";
+    $verified_password = "";
     $fechaNac = "";
     $telefono = "";
     $preferencias = "";
     $genero = "";
     $id_persona = 0;
+    $terminos = "";
 
     if(isset($_POST['id_persona'])){$id_persona = $_POST['id_persona'];}
     
@@ -22,6 +50,8 @@ if(isset($_POST['btnRegistrarse'])){
     if(isset($_POST['email'])){$email = $_POST['email'];}
     
     if(isset($_POST['password'])){$password = $_POST['password'];}
+
+    if(isset($_POST['verified-password'])){$verified_password = $_POST['verified-password'];}
     
     if(isset($_POST['fechaNac'])){$fechaNac = $_POST['fechaNac'];}
     
@@ -31,19 +61,26 @@ if(isset($_POST['btnRegistrarse'])){
     
     if(isset($_POST['genero'])){$genero = $_POST['genero'];}
 
-    if(strlen(trim($nombre)) >= 5 && strlen(trim($cedula)) >= 7 && strlen(trim($email)) >= 8 && $password != "" && strlen(trim($fechaNac)) == 10 && $telefono != "" && $genero != "" ){
+    if(isset($_POST['terminos'])){$terminos = $_POST['terminos'];}
+
+    if(strlen(trim($nombre)) >= 5 && strlen(trim($cedula)) >= 7 && strlen(trim($email)) >= 8 && strlen(trim($fechaNac)) == 10 && $telefono != "" && $genero != "" && $terminos != "" ){
         // echo '<pre>';
         // print_r($_POST);
         // echo '</pre>';
         if($id_persona == 0){
+            if($password != $verified_password || empty($password)){
+                echo "La constraseñas no coinciden ";
+                return;
+            }
             try{
                 $sqlInse = "INSERT INTO t_datos_personales (nombre_completo, cedula, correo, contrasena, fecha_nacimiento, telefono, genero, generos_fav )
                 VALUES(:nombre, :cedula, :email, :contrasena, :fecha_nacimiento, :telefono, :genero, :generos_fav )";
+                $passwordHash = password_hash($password, PASSWORD_DEFAULT);
                 $stmt = $pdo->prepare($sqlInse);
                 $stmt->bindParam(":nombre", $nombre);
                 $stmt->bindParam(":cedula", $cedula);
                 $stmt->bindParam(":email", $email);
-                $stmt->bindParam(":contrasena", $password);
+                $stmt->bindParam(":contrasena", $passwordHash);
                 $stmt->bindParam(":fecha_nacimiento", $fechaNac);
                 $stmt->bindParam(":telefono", $telefono);
                 $stmt->bindParam(":genero", $genero);
@@ -65,17 +102,34 @@ if(isset($_POST['btnRegistrarse'])){
                 genero = :genero, 
                 generos_fav = :generos_fav
                 WHERE id_persona = :id_persona";
+                if(!empty($password)){
+                    if($password != $verified_password){
+                        echo "Las contraseñas no coinciden. ";
+                        return;
+                    }
+                    $passwordHash = password_hash($password, PASSWORD_DEFAULT);
+                    
+                }else{
+                    if(!isset($persona)){
+                        $sql = "SELECT contrasena FROM t_datos_personales WHERE id_persona = :id";
+                        $stmt = $pdo->prepare($sql);
+                        $stmt->bindParam(":id", $id_persona);
+                        $stmt->execute();
+                        $persona = $stmt->fetch(PDO::FETCH_ASSOC);
+                    }
+                    $passwordHash = $persona['contrasena'];
+                }
                 $stmt = $pdo->prepare($sqlUpt);
                 
                 $stmt->bindParam(":id_persona", $id_persona);
-                $stmt->bindParam(":nombre", $nombre);
+                $stmt->bindParam(":nombre_completo", $nombre);
                 $stmt->bindParam(":cedula", $cedula);
                 $stmt->bindParam(":correo", $email);
-                $stmt->bindParam(":contrasena", $password);
-                $stmt->bindParam(":fechaNac", $fechaNac);
+                $stmt->bindParam(":contrasena", $passwordHash);
+                $stmt->bindParam(":fecha_nacimiento", $fechaNac);
                 $stmt->bindParam(":telefono", $telefono);
                 $stmt->bindParam(":genero", $genero);
-                $stmt->bindParam(":preferencias", $preferencias);
+                $stmt->bindParam(":generos_fav", $preferencias);
                 $stmt->execute();
                 echo"Datos actualzados correctamente. ";
             } catch(PDOException $e){
@@ -114,46 +168,46 @@ if(isset($_POST['btnRegistrarse'])){
             <h1>Formulario de Registro</h1>
 
             <label for="nombre">Nombre Completo:
-                <input type="text" id="nombre" name="nombre" required>
+                <input type="text" id="nombre" name="nombre" value="<?php echo isset($persona) ? htmlspecialchars($persona['nombre_completo']) : ''; ?>" required>
                 <input type="hidden" name="id_persona" value="<?php echo isset($persona) ? $persona['id_persona'] : 0; ?>">
             </label>
             <label for="cedula">Cedula:
-                <input type="text" id="cedula" name="cedula" required>
+                <input type="text" id="cedula" name="cedula" value="<?php echo isset($persona) ? htmlspecialchars($persona['cedula']): ''; ?>" required>
             </label>
             <label for="email">Correo Electrónico:
-                <input type="email" id="email" name="email" required>
+                <input type="email" id="email" name="email" value="<?php echo isset($persona) ? htmlspecialchars($persona['correo']) : ''; ?>" required>
                 <p id="error-email"></p>
             </label>
             <label for="password">
                 Contraseña:
-                <input type="password" id="password" name="password" required>
+                <input type="password" id="password" name="password" >
             </label>
             <label for="verified-password">
                 Confirmar contraseña:
-                <input type="password" id="verified-password" name="verified-password" required>
+                <input type="password" id="verified-password" name="verified-password" >
                 <p id="error-pass"></p>
             </label>
             <label for="fechaNac">Fecha de nacimiento:
-                <input type="date" id="fechaNac" name="fechaNac" required>
+                <input type="date" id="fechaNac" name="fechaNac" value="<?php echo isset($persona) ? htmlspecialchars($persona['fecha_nacimiento']) : ''; ?>" required>
             </label>
             <label for="telefono">Teléfono:
-                <input type="tel" id="telefono" name="telefono" required>
+                <input type="tel" id="telefono" name="telefono" value="<?php echo isset($persona) ? htmlspecialchars($persona['telefono']) : ''; ?>" required>
             </label>
             <fieldset>
                 <legend>Género</legend>
                 <label for="generoM">
                     Masculino
-                    <input type="radio" id="generoM" name="genero" value="Masculino" required>
+                    <input type="radio" id="generoM" name="genero" value="Masculino" <?php if(isset($persona) && $persona['genero']=="Masculino") echo "checked"; ?> required>
                 </label>
                 <label for="generoF">
                     Femenino
-                    <input type="radio" id="generoF" name="genero" value="femenino">
+                    <input type="radio" id="generoF" name="genero" value="Femenino" <?php if(isset($persona) && $persona['genero']=="Femenino") echo "checked"; ?>>
                 </label>
             </fieldset>
             <label for="preferencias">
                 Géneros favoritos:
                 <textarea name="preferencias" id="preferencias"  maxlength="35"
-                    placeholder="Ej: Acción, Comedia, Drama."></textarea>
+                    placeholder="Ej: Acción, Comedia, Drama."><?php echo isset($persona) ? htmlspecialchars($persona['generos_fav']) : '';?></textarea>
             </label>
             <label for="terminos">
                 <input type="checkbox" id="terminos" name="terminos" required>
@@ -167,7 +221,7 @@ if(isset($_POST['btnRegistrarse'])){
     <div class="table-cont">
         <table id="user-table" class="user-table" >
             <tr>
-                <th>Editar</th>
+                <th>&nbsp;</th>
                 <th>Nombre Completo</th>
                 <th>Cedula</th>
                 <th>Correo Electrónico</th>
@@ -175,30 +229,29 @@ if(isset($_POST['btnRegistrarse'])){
                 <th>Telefono</th>
                 <th>Genero</th>
                 <th>Preferencias</th>
-                <th>Eliminar</th>
                 <th>&nbsp;</th>
             </tr>
-            <tr>
-                <?php 
-                    $sql = "SELECT * FROM t_datos_personales ORDER BY nombre_completo";
-                    $stmt = $pdo->prepare($sql);
-                    $stmt->execute();
-                    $PERSONAS = $stmt->fetchAll(PDO::FETCH_ASSOC);
-                    foreach($PERSONAS as $PERSONA){
-                        echo "<tr>";
-                            echo "<td><a href='?editar=".$PERSONA['id_persona']."'>Editar</a></td>";
-                            echo "<td>". $PERSONA['nombre_completo'] ."</td>";
-                            echo "<td>". $PERSONA['cedula'] ."</td>";
-                            echo "<td>". $PERSONA['correo'] ."</td>";
-                            echo "<td>".date("y-m-d",strtotime($PERSONA['fecha_nacimiento']))  ."</td>";
-                            echo "<td>". $PERSONA['telefono'] ."</td>";
-                            echo "<td>". $PERSONA['genero'] ."</td>";
-                            echo "<td>". $PERSONA['generos_fav'] ."</td>";
-                            echo "<td><a href='?eliminar=".$PERSONA['id_persona']."'>Eliminar</a></td>";
-                        echo "</tr>";
-                    }
-                ?>
-            </tr>
+            
+            <?php 
+                $sql = "SELECT * FROM t_datos_personales ORDER BY nombre_completo";
+                $stmt = $pdo->prepare($sql);
+                $stmt->execute();
+                $PERSONAS = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                foreach($PERSONAS as $PERSONA){
+                    echo "<tr>";
+                        echo "<td><a href='?editar=".$PERSONA['id_persona']."'>Editar</a></td>";
+                        echo "<td>". htmlspecialchars($PERSONA['nombre_completo']) ."</td>";
+                        echo "<td>". htmlspecialchars($PERSONA['cedula']) ."</td>";
+                        echo "<td>". htmlspecialchars($PERSONA['correo']) ."</td>";
+                        echo "<td>".date("y-m-d",strtotime($PERSONA['fecha_nacimiento']))  ."</td>";
+                        echo "<td>". htmlspecialchars($PERSONA['telefono']) ."</td>";
+                        echo "<td>". htmlspecialchars($PERSONA['genero']) ."</td>";
+                        echo "<td>". htmlspecialchars($PERSONA['generos_fav']) ."</td>";
+                        echo "<td><a href='?eliminar=".$PERSONA['id_persona']."'>Eliminar</a></td>";
+                    echo "</tr>";
+                }
+            ?>
+            
             
         </table>
     </div>
